5fe82abb0c16d6f88127ed4c46c299e8c0e821f4
Yury Groza <yurygro07@gmail.com>
2023-02-24 19:23:48 +0100

    Quart Commit

--- app/src/main/java/com/example/uf1_nf1_pr2_activities_fragments_i_mvvm_i_firebase_yurygroza/MainActivity.java
+++ app/src/main/java/com/example/uf1_nf1_pr2_activities_fragments_i_mvvm_i_firebase_yurygroza/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             mTvDadesCompartides.setText(itemObservat);
         });
         CarregaFragment(new Fragment1());
-        //btn
+        //btn vv
         mBtnFragment1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
